d788f492542281df4133893f4bb009012bd91b57
T.N.T. <ghi_user@hotmail.de>
2014-10-18 18:54:53 +0200

    Test commit!

---
+++ ctest/src/ctest.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : ctest.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("Wie geht es dir?");
+	return EXIT_SUCCESS;
+}
